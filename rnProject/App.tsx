fd2db3b298c8aad0271a5be00effb6b6a222806f
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-04 23:32:27 +0900

    react-native 개발환경 구축

---
+++ rnProject/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = (): React.JSX.Element => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
